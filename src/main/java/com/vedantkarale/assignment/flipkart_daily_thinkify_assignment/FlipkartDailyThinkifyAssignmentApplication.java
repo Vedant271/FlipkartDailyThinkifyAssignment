fcb181368386517edbf683bbb024f7cab771a712
Vedant Karale <vedantkarale271@gmail.com>
2025-06-10 08:49:07 +0530

    Flipkart Daily Base

---
+++ src/main/java/com/vedantkarale/assignment/flipkart_daily_thinkify_assignment/FlipkartDailyThinkifyAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.vedantkarale.assignment.flipkart_daily_thinkify_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlipkartDailyThinkifyAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlipkartDailyThinkifyAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vedantkarale/assignment/flipkart_daily_thinkify_assignment/FlipkartDailyThinkifyAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vedantkarale.assignment.flipkart_daily_thinkify_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlipkartDailyThinkifyAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
